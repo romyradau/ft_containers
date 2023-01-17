a3474d8a864780aa415d95efb76515f3571f92aa
Romy Schleicher <rschleic@2-C-4.42heilbronn.de>
2023-01-17 16:25:49 +0100

    Canonical form done, testing implemented

--- incl/romyVector.hpp
+++ incl/romyVector.hpp
@@ -8,175 +8,152 @@
 #include <stdexcept>
 #include <iterator>
 
-namespace ft{}
-using namespace ft;
-//geht das so oder muss ich's normal eingliedern?
+namespace ft{
+	//TODO:geht das so oder muss ich's normal eingliedern?
 
-//The typedef-names are aliases for existing types, and are not declarations of new types.
-template<typename T, class Alloc = std::allocator<T>>
-class Vector{
-	public: 
-	typedef	T									value_type;
-	typedef	Alloc								allocator_type;
-	typedef	size_t								size_type;
-	
-	//originally allocator_type::reference why then value_type& and not Alloc&??
-	//->the standard default allocator, automatically returns &x
-	//so it's basically the same!
-	typedef	value_type&							reference;
-	typedef const value_type&					const_reference;
-	typedef	value_type*							pointer;
-	typedef	const value_type*					const_pointer;
-
-	//extra header for the iterators
-	typedef ft::vec_iterator<value_type>		iterator;
-	typedef ft::vec_iterator<const value_type>	const_iterator;
-	typedef	ft::vec_iterator<value_type>		reverse_iterator;
-	typedef ft::vec_iterator<const value_type>	const_reverse_iterator;
-
-	// difference_type	a signed integral type, identical to: iterator_traits<iterator>::difference_type
-	// will be in iterator traits
-
-	//non-members, how does one know them??
-	private:
-		size_type								_cap; //avaialble space
-		size_type								_size; //actual space
-		allocator_type							_alloc; //allocator object used by vector
-		pointer									_pointer; //pointer to vector
-
-	public:
-		explicit Vector (const allocator_type& alloc = allocator_type());
-		explicit Vector (size_type n, const value_type& val = value_type(), const allocator_type& alloc = allocator_type());
-
-		//TODO: den vielleicht erst nach IteratorImplementierung?
-		template <class InputIterator>         Vector (InputIterator first, InputIterator last, const allocator_type& alloc = allocator_type());
-		Vector (const vector& x);
-
-		Vector& operator= (const vector& x);
-};
-
-//CINSTRUCT VECTOR
-//Constructs an empty container, with no elements.
-//allocator_type() ruft den constructor vom std::allocator auf
-//TODO:die schreibweise, den Zusammenhang verstehe ich nicht, warum muss ich bei vector <T, Alloc> angeben??
-template<typename T, class Alloc>
-Vector<T, Alloc>::Vector(const allocator_type &alloc){
-	_cap = 0;
-	_size = 0;
-	_alloc = alloc; //inbuilt it knows that it looks for std::allocator<T>
-	_pointer = NULL;
-}
-
-//Constructs a container with n elements. Each element is a copy of val.
-//mit value_type() ist T vom vector gemeint == hier werden z.b. 5, int allociert
-template<typename T, class Alloc>
-Vector<T, Alloc>::Vector(size_type n, const value_type& val, const allocator_type &alloc){
-	_cap = n;//TODO: sollte das hier nicht mehr sein, von Anfang an?
-	_size = n;	
-	_alloc = alloc; //inbuilt it knows that it looks for std::allocator<T>
-	_pointer = this->_alloc.allocate(n); // allocated aber nicht constructed
-	for (size_t i = 0; i < n; i++)
-		this->_alloc.construct(_pointer + i, val);
-}
-
-//shallow copy constructor
-template<typename T, class Alloc>
-Vector<T, Alloc>::Vector(const vector& x){
-	_cap = 0;
-	_size = 0;
-	_alloc = std::allocator<T>;
-	_pointer = NULL;
-	*this = x;
-	//TODO: warum muss erstmal leer initialisiert werden
-}
-
-//ASSIGN CONTENT
-//TODO::warum muss man immer den spezifischen vektor angeben??vector<T, Alloc
-template<typename T, class Alloc>
-Vector<T, Alloc>& Vector<T, Alloc>::operator=(const Vector<T, Alloc>& x){
-	
-}
-
-//VECTOR DESTRUCTOR
-template<typename T, class Alloc>
-Vector<T, Alloc>::~Vector(){
-	for(size_t i = 0; i < this->_size; i++)
-		this->_alloc.destroy(&this->_pointer[i]); //destroyed aber noch nicht deallocated
-	this->_alloc.deallocate(this->_pointer, this->_capacity); // braucht kein [], deallocated so viele items wie einsta allocated
-}
-
-
-// template<typename T>
-// class Vector
-// {
-// 	public:
-// 		Vector(){
-// 			ReAlloc(2);
-// 		}
-// 		//wie wird in STL allociert?
-// 		~Vector(){
-// 			delete [] m_Data;
-// 			//also make sure to delete the pointers?
-// 			//loscht nicht die elemente sondern ???
-// 		}
-
-// 		void PushBack(const T& value){
-// 			if (m_Size >= m_Capacity)//why not just >
-// 				ReAlloc(m_Capacity + m_Capacity / 2);
-// 			m_Data[m_Size] = value;
-// 			m_Size++;
-// 		}
-
-// 		void PopBack(){
-// 			if (m_Size > 0){
-// 				m_Size--;
-// 				m_Data[m_Size].~T();//calls the explicit deconstructor of the type
-// 			}
-// 		}
-
-// 		void Clear(){
-// 			for(size_t i = 0; i < m_Size; i++)
-// 				m_Data[i].~T();
-// 		}
+	//The typedef-names are aliases for existing types, and are not declarations of new types.
+	template<typename T, class Alloc = std::allocator<T> >
+	class Vector{
+		public: 
+		typedef	T									value_type;
+		typedef	Alloc								allocator_type;
+		typedef	size_t								size_type;
 		
-// 		T const & operator[] (size_t index) const{
-// 			return m_Data[index];
-// 			//TODO:check what STL does if index is not available!
-// 		}
-// 		//access a vector element via a pointer, which already has the [ operator ]
-// 		//readOnly
-		
-// 		T& operator[] (size_t index){
-// 			return m_Data[index];
-// 		}
-// 		//readAndWrite
+		//originally allocator_type::reference why then value_type& and not Alloc&??
+		//->the standard default allocator, automatically returns &x
+		//so it's basically the same!
+		typedef	value_type&							reference;
+		typedef const value_type&					const_reference;
+		typedef	value_type*							pointer;
+		typedef	const value_type*					const_pointer;
 
-// 		size_t Size() const{
-// 			return m_Size;
-// 		}
+		//extra header for the iterators
+		// typedef ft::vec_iterator<value_type>		iterator;
+		// typedef ft::vec_iterator<const value_type>	const_iterator;
+		// typedef	ft::vec_iterator<value_type>		reverse_iterator;
+		// typedef ft::vec_iterator<const value_type>	const_reverse_iterator;
 
-// 	private:
-// 		void ReAlloc(size_t newCapacity){
+		// difference_type	a signed integral type, identical to: iterator_traits<iterator>::difference_type
+		// will be in iterator traits
 
-// 			T* newVector = new T[newCapacity];
-// 			//warum muss man fuer weniger space trotzdem allocieren?
+		private:
+			size_type								_cap; //avaialble space
+			size_type								_size; //actual space
+			allocator_type							_alloc; //allocator object used by vector
+			pointer									_pointer; //pointer to vector
 
-// 			if (newCapacity < m_Size)
-// 				m_Size = newCapacity;
-				
-// 			//will be changed to cumstom allocator here
-// 			for (size_t i = 0; i < m_Size; i++){
-// 				newVector[i] = m_Data[i];
-// 				//this is copying -> moving would be better
-// 			}
-// 			delete [] m_Data;
-// 			m_Data = newVector;
-// 			m_Capacity = newCapacity;
-// 		}
+		public:
+			explicit Vector (const allocator_type& alloc = allocator_type());
+			explicit Vector (size_type n, const value_type& val = value_type(), const allocator_type& alloc = allocator_type());
 
-// 	private:
-// 		T* 		m_Data = nullptr;
+			//TODO: den vielleicht erst nach IteratorImplementierung?
+			// template <class InputIterator>         Vector (InputIterator first, InputIterator last, const allocator_type& alloc = allocator_type());
+			Vector (const Vector& x);
 
-// 		size_t	m_Size = 0;
-// 		size_t	m_Capacity = 0;
-// };
+			Vector& operator= (const Vector& x);
+			~Vector();
+
+	//functions needed for testing
+			size_type 		size() const{return this->_size;}
+			reference 		operator[](size_type n){return this->_pointer[n];}
+			const_reference operator[](size_type n) const{return this->_pointer[n];}
+			void clear();
+			void reserve (size_type n);
+			// Portable programs should never call this function with an argument n that is out of range, since this causes undefined behavior.
+
+
+	};
+
+	//CINSTRUCT VECTOR
+	//Constructs an empty container, with no elements.
+	//allocator_type() ruft den constructor vom std::allocator auf
+	//TODO:die schreibweise, den Zusammenhang verstehe ich nicht, warum muss ich bei vector <T, Alloc> angeben??
+	template<typename T, class Alloc>
+	Vector<T, Alloc>::Vector(const allocator_type &alloc){
+		_cap = 0;
+		_size = 0;
+		_alloc = alloc; //inbuilt it knows that it looks for std::allocator<T>
+		_pointer = NULL;
+		printf("hallo\n");
+	}
+
+	//Constructs a container with n elements. Each element is a copy of val.
+	//mit value_type() ist T vom vector gemeint == hier werden z.b. 5, int allociert
+	template<typename T, class Alloc>
+	Vector<T, Alloc>::Vector(size_type n, const value_type& val, const allocator_type &alloc){
+		_cap = n;
+		_size = n;	
+		_alloc = alloc; //inbuilt it knows that it looks for std::allocator<T>
+		_pointer = this->_alloc.allocate(n); // allocated aber nicht constructed
+		for (size_t i = 0; i < n; i++)
+			this->_alloc.construct(_pointer + i, val);
+	}
+
+	//shallow copy constructor
+	template<typename T, class Alloc>
+	Vector<T, Alloc>::Vector(const Vector& x){
+		_cap = 0;
+		_size = 0;
+		_alloc = allocator_type();//TODO:?
+		_pointer = NULL;
+		*this = x;
+	}
+	//TODO: remember assign ist nicht implemented desween wird ne shallow copy gemacht udn deswegen fuckt malloc ab
+	//wenn du dann mal operator= hast dann kannst du dir damit selbst beweisen ob das ne deep copy ist oder nciht!
+
+	//ASSIGN CONTENT WITH FUNCTIONS
+	// template<typename T, class Alloc>
+	// Vector<T, Alloc> &Vector<T, Alloc>::operator=(const Vector<T, Alloc>& x){
+	// 	if (*this != x)
+	// 		assign(x.begin(), x.end());
+	// 	return *this;
+	// }
+
+	//ASSIGN CONTENT WITHOUT
+	//Assigns new contents to the container, replacing its current contents, and modifying its size accordingly.
+	template<typename T, class Alloc>
+	Vector<T, Alloc> &Vector<T, Alloc>::operator=(const Vector<T, Alloc>& x){
+		clear();//Removes all elements from the vector (which are destroyed), leaving the container with a size of 0.
+		if (this->_cap < x._size)
+			reserve(x._cap);
+		this->_size = x._size;
+		for (size_type i = 0; i < x._size; i++)
+			this->_alloc.construct(&this->_pointer[i], x._pointer[i]);	
+		return *this;
+	}
+
+	//VECTOR DESTRUCTOR
+	template<typename T, class Alloc>
+	Vector<T, Alloc>::~Vector(){
+		for(size_t i = 0; i < this->_size; i++)
+			this->_alloc.destroy(&this->_pointer[i]); //destroyed aber noch nicht deallocated
+		this->_alloc.deallocate(this->_pointer, this->_cap); // braucht kein [], deallocated so viele items wie einsta allocated
+	}
+
+	template<typename T, class Alloc>
+	void 		Vector<T, Alloc>::clear(){
+		//TODO: vielleicht muss heir noch ne if Bedingung hin??
+		for(size_t i = 0; i < this->_size; i++)
+			this->_alloc.destroy(this->_pointer + i);
+		this->_size = 0;
+	}
+
+
+	template<typename T, class Alloc>
+	void 		Vector<T, Alloc>::reserve(size_type n){
+		//TODO::The function throws length_error if n is greater than max_size.
+		if (this->_cap < n){
+			pointer	tmp;
+			tmp = this->_alloc.allocate(n);
+			for (size_t i = 0; i < this->_size; i++){
+				this->_alloc.construct(tmp + i, this->_pointer[i]);
+				this->_alloc.destroy(this->_pointer + i);
+			}
+			this->_alloc.deallocate(this->_pointer, this->_cap);
+			this->_cap = n;
+			this->_pointer = tmp;
+		}
+	}
+
+
+}
--- incl/simplemain.cpp
+++ incl/simplemain.cpp
@@ -1,25 +1,29 @@
 #include "romyVector.hpp"
+#include <vector>
+#define Vector std::vector
 
+using namespace ft;
 //extra function to print out our Vector with yet unknown type
 template<typename T>
 void PrintVector( const Vector<T>& vector){
-	for( size_t i = 0; i < vector.Size(); i++)
+	for( size_t i = 0; i < vector.size(); i++)
 		std::cout << vector[i] << std::endl;
 }
 
+
+
 int main(){
 
-	Vector<std::string> vector;
-	vector.PushBack("Hit");
-	vector.PushBack("them");
-	vector.PushBack("vector");
-	vector.PopBack();
-	vector.PushBack("Hit");
-	vector.PushBack("them");
-	vector.PopBack();
-	vector.PushBack("vector");
-	vector.Clear();
+	Vector<int> first;                                // empty vector of ints
+  	Vector<int> second (2,100);                       // four ints with value 100
+  	// Vector<int> third (second.begin(),second.end());  // iterating through second
+  	Vector<int> fourth (second);
+	// vector.PushBack("Hit");
+	// vector.PushBack("them");
+	// vector.PushBack("vector");
 
-	vector.PushBack("vector");
-	PrintVector(vector);
+	PrintVector(first);
+	PrintVector(second);
+	PrintVector(fourth);
+
 }
