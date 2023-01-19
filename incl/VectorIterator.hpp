9bb460d3530f03015ab29158671e94efadc1f420
Romy Schleicher <rschleic@3-B-7.42heilbronn.de>
2023-01-19 14:32:03 +0100

    too many iterator

--- incl/VectorIterator.hpp
+++ incl/VectorIterator.hpp
@@ -18,15 +18,15 @@ namespace ft{
 	template<typename T>//will be put as parameter next to class declaration
 	class vec_iterator{
 		// C++ expects some properties from an iterator:
-		using value_type = T
+		using value_type		= T
 		// using value_type = typename Vector::value_type
-		using pointer = value_type*;
-		using const_pointer = const pointer;
-		using reference = value_type&;
-		using const_reference = const reference;
-		using difference_type = std::ptrdiff_t;// It is a type able to represent the result of any valid pointer subtraction operation.
+		using pointer			= value_type*;
+		using const_pointer		= const pointer;
+		using reference			= value_type&;
+		using const_reference	= const reference;
+		using difference_type 	= std::ptrdiff_t;// It is a type able to represent the result of any valid pointer subtraction operation.
 		using iterator_category = std::random_access_iterator_tag;// Empty class to identify the category of an iterator as a random-access iterator:
-		using const_iterator = vec_iterator< const value_type >;
+		using const_iterator	= vec_iterator< const value_type >;
 
     	// typedef Category  iterator_category;
 		// typedef T		  value_type;
@@ -34,35 +34,38 @@ namespace ft{
     	// typedef Reference reference;
     	// typedef Distance  difference_type;
 
+		// https://cplusplus.com/reference/iterator/RandomAccessIterator/
 		public:
-			TODO://do i need the empty constructor??
-			vec_iterator(void): it_ptr() {}
+			// vec_iterator(void): it_ptr() {}//not sure if needed
 			vec_iterator(pointer ptr): it_ptr(ptr) {}
 			TODO://wie sieht das nochmal ausgeschrieben aus?
 			vec_iterator(vec_iterator const & src) : it_ptr(src.it_ptr) {}
-			~vec_iterator(void) {}
-
-			vec_iterator& operator=(vec_iterator const & rhs) {
-				if (this != &rhs)
-					this->it_ptr = rhs.it_ptr;
-				return *this;
+			vec_iterator& 				operator=(vec_iterator const & rhs) {
+										if (this != &rhs)
+											this->it_ptr = rhs.it_ptr;
+										return *this;
 			}
-  			vec_iterator& operator++() {++it_ptr;return *this;}
-  			vec_iterator operator++(int) {vec_iterator tmp(*this); operator++(); return tmp;}
-			//post operator
-			//returns a copy of the it, bc the object in place should not be modified
-			vec_iterator& operator--() {--it_ptr;return *this;}
-  			vec_iterator operator--(int) {vec_iterator tmp(*this); operator--(); return tmp;}
-			reference operator[](int index) {return *(it_ptr + index);}
-			pointer	operator->() {return it_ptr;}
-			reference operator*() {return *it_ptr;}
-  			bool operator==(vec_iterator const & rhs) const {return it_ptr==rhs.it_ptr;}
-			//goes to the bool==operator of teh pointer object
-  			bool operator!=(vec_iterator const & rhs) const {return it_ptr!=rhs.it_ptr;}
-			// vec_iterator& operator+(int n) {return }
-			vec_iterator& operator+=(difference_type n) {this.it_ptr += n; return *this}
-			// vec_iterator& operator-(int n)
-			vec_iterator& operator-=(difference_type n) {this.it_ptr -= n; return *this}
+			//TODO:wie kann man das in einer Zeile schreiben?
+			~vec_iterator(void) {}
+  			bool 						operator==(vec_iterator const & rhs) const {return it_ptr==rhs.it_ptr;}//goes to the bool==operator of teh pointer object
+  			bool 						operator!=(vec_iterator const & rhs) const {return it_ptr!=rhs.it_ptr;}
+			pointer						operator->() {return it_ptr;}
+			reference 					operator*() {return *it_ptr;}
+			//*a = t
+  			vec_iterator& 				operator++() {++it_ptr;return *this;}
+  			vec_iterator 				operator++(int) {vec_iterator tmp(*this); operator++(); return tmp;}//post operator//returns a copy of the it, bc the object in place should not be modified
+			vec_iterator& 				operator--() {--it_ptr;return *this;}
+  			vec_iterator 				operator--(int) {vec_iterator tmp(*this); operator--(); return tmp;}
+			//*a++, *a--
+			//a + n, n + a, a - n, a - b
+			//a < b,a > b,a <= b,a >= b
+			reference 					operator[](int index) {return *(it_ptr + index);}
+			vec_iterator& 				operator+=(difference_type n) {this.it_ptr += n; return *this}
+			vec_iterator& 				operator-=(difference_type n) {this.it_ptr -= n; return *this}
+    		vec_iterator  				operator+ (difference_type n) const {return vec_iterator(this->it_ptr + n);}
+    		vec_iterator 				operator- (difference_type n) const {return vec_iterator(this->it_ptr - n);}
+    		// vec_iterator 				base() const {return this->it_ptr;}
+
 
 			TODO://which ones are missing?
 			//TODO: was macht diese base function genau?
